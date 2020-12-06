e8e71573cda5e30d37f307c99daa251fe6d462f4
KKang <1473255158@qq.com>
2020-12-06 17:22:20 +0800

    废线

--- app/src/main/java/com/projectlite2/android/activity/SplashActivity.java
+++ app/src/main/java/com/projectlite2/android/activity/SplashActivity.java
@@ -18,7 +18,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
         StatusBarUtil.setTransparent(SplashActivity.this);
 
